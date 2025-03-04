9ca34539e5b835ac68674342bca08e249bcbe61f
Garvit Singla <garvits093@gmail.com>
2025-03-04 15:32:30 +0530

    first commit

--- apps/ws_backend/src/index.ts
+++ apps/ws_backend/src/index.ts
@@ -1,4 +1,6 @@
+import {JWT_SECRET} from "backendcommon"
 import {WebSocketServer} from "ws"
+import jwt from "jsonwebtoken"
 
 const wss = new WebSocketServer({port: 8080})
 
@@ -9,9 +11,40 @@ wss.on("connection", (ws,reqest) => {
     return
   }
   const token = url.split("=")[1]
-  console.log(token)
-  console.log(typeof token)
-  ws.on("message", (message) => {
     
+  if (!token){
+    ws.close()
+    return
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET)
+  console.log(decoded)
+  if (!decoded){
+    ws.close()
+    return
+  }
+
+let request
+  ws.on("message", (message) => { 
+    
+    try {
+      if (typeof message == "string"){
+        request = JSON.parse(message)
+      }else{
+        request = JSON.parse(message.toString())
+      }
+
+
+      
+    } catch (error) {
+      ws.close()
+      return
+    }
+    
+
   })
+  } catch (error) {
+    ws.close()
+  }
 })
